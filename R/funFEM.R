612e529570174dc0b2ad2a986b97b7422c22a709
Charles Bouveyron <charles.bouveyron@gmail.com>
2021-10-27 13:10:11 +0000

    version 1.2

--- R/funFEM.R
+++ R/funFEM.R
@@ -1,5 +1,5 @@
 funFEM <-
-function(fd,K=2:6,model='AkjBk',crit='bic',init='hclust',Tinit=c(),maxit=50,eps=1e-6,disp=FALSE,lambda=0,graph=FALSE){
+function(fd,K=2:6,model='AkjBk',crit='bic',init='kmeans',Tinit=c(),maxit=50,eps=1e-6,disp=FALSE,lambda=0,graph=FALSE){
   call = match.call()
   MOD = c('DkBk','DkB','DBk','DB','AkjBk','AkjB','AkBk','AkB','AjBk','AjB','ABk','AB','all')
   CRI = c('bic','aic','icl')
@@ -7,6 +7,7 @@ function(fd,K=2:6,model='AkjBk',crit='bic',init='hclust',Tinit=c(),maxit=50,eps=
   if (any(!model%in%MOD)) stop("Invalid model name\n",call.=FALSE)
   if (any(!crit%in%CRI)) stop("Invalid criterion name\n",call.=FALSE)
   if (any(!init%in%INIT)) stop("Invalid initialization name\n",call.=FALSE)
+  if (any(1%in%K)) stop("K = 1 is not allowed at the moment!\n",call.=FALSE)
   # if (init=='hclust' & nrow(Y)>5000) stop('Too much data for this initialization',,call.=FALSE)
   # if (K>=ncol(Y)) stop("K must be strictly less than the number of variables",call.=FALSE)
   if (length(model)==1) if (model=='all') model = c('DkBk','DkB','DBk','DB','AkjBk','AkjB','AkBk','AkB','AjBk','AjB','ABk','AB')
@@ -30,11 +31,12 @@ function(fd,K=2:6,model='AkjBk',crit='bic',init='hclust',Tinit=c(),maxit=50,eps=
           if (crit=='icl') cat(model[i],'\t:\t icl =',resultat[[it]]$icl,'\n')
           #if (crit=='fisher') cat(MOD[i],'\t:\t fish =',resultat[[(k-1)*length(model)+i]]$fish)
         }
-        it = it + 1
       }
+      it = it + 1
     }
-    if (disp) cat('\n')
+    #if (disp) cat('\n')
   }
+  if (prod(is.na(bic))==1) stop("No reliable results to return (empty clusters in all partitions)!")
   if (crit=='bic'){ id_max = which.max(bic); crit_max = resultat[[id_max]]$bic}
   if (crit=='aic'){ id_max = which.max(aic); crit_max = resultat[[id_max]]$aic}
   if (crit=='icl'){ id_max = which.max(icl); crit_max = resultat[[id_max]]$icl}
@@ -43,7 +45,9 @@ function(fd,K=2:6,model='AkjBk',crit='bic',init='hclust',Tinit=c(),maxit=50,eps=
   if (disp) cat('The best model is',res$model,'with K =',res$K,'(',crit,'=',crit_max,')\n')
   res$crit = crit
   nm = length(model)
-  res$plot = list(bic=matrix(bic,ncol=nm,byrow=T),aic=matrix(aic,ncol=nm,byrow=T),icl=matrix(icl,ncol=nm,byrow=T),K=K,nbprm=matrix(nbprm,ncol=nm,byrow=T),ll=matrix(ll,ncol=nm,byrow=T),models=model)
+  res$allCriterions = data.frame(K,model,bic=matrix(bic,ncol=nm,byrow=T),aic=matrix(aic,ncol=nm,byrow=T),
+                            icl=matrix(icl,ncol=nm,byrow=T),nbprm=matrix(nbprm,ncol=nm,byrow=T),
+                            ll=matrix(ll,ncol=nm,byrow=T))
   res$call = call
   class(res)='fem'
   res
